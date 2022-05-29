ef8e47f6d7044fdfec2080a2b3dfaf57547907cc
egnomerator <egnomerator@users.noreply.github.com>
2022-05-29 10:28:45 -0500

    added more devs to starting list

--- ControllerWithSimpleInMemoryDb/InMemoryDb/DevDb.cs
+++ ControllerWithSimpleInMemoryDb/InMemoryDb/DevDb.cs
@@ -5,6 +5,13 @@ namespace ControllerWithSimpleInMemoryDb.InMemoryDb
 {
     public class DevDb
     {
-        public static readonly Dictionary<long, Dev> Devs = new Dictionary<long, Dev> { { 1, new Dev { Id = 1, Name = "Charlie" } } };
+        public static readonly Dictionary<long, Dev> Devs = new Dictionary<long, Dev> 
+        { 
+            { 1, new Dev { Id = 1, Name = "Charlie" } },
+            { 2, new Dev { Id = 2, Name = "Mac" } },
+            { 3, new Dev { Id = 3, Name = "Dennis" } },
+            { 4, new Dev { Id = 4, Name = "Frank" } },
+            { 5, new Dev { Id = 5, Name = "Dee" } }
+        };
     }
 }
